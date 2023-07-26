31085f571c0653bdf9aab78ac822964098762ad5
amine <mohamedamineguesmi@insat.u-carthage.tn>
2023-07-26 11:58:29 +0100

    add build

---
+++ build/precache-manifest.d018914b88dc234aa0dd9fe5c053d344.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "941093959c5e500bfa32",
+    "url": "/static/css/main.524c60cf.chunk.css"
+  },
+  {
+    "revision": "941093959c5e500bfa32",
+    "url": "/static/js/main.94109395.chunk.js"
+  },
+  {
+    "revision": "243695e79077f0c56752",
+    "url": "/static/js/1.243695e7.chunk.js"
+  },
+  {
+    "revision": "30a467570396444d8741",
+    "url": "/static/css/2.869f6427.chunk.css"
+  },
+  {
+    "revision": "30a467570396444d8741",
+    "url": "/static/js/2.30a46757.chunk.js"
+  },
+  {
+    "revision": "870048a51ef18d793a40",
+    "url": "/static/js/runtime~main.870048a5.js"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "11c9c663477ce6439628b381c8a2c601",
+    "url": "/static/media/logo.11c9c663.png"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "43f484d6860e0efb5d190d03d3b71d1b",
+    "url": "/static/media/nav-icon4.43f484d6.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "9cbddd3b237f46f0a03d642b1f7ff673",
+    "url": "/static/media/Banner.9cbddd3b.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "4d1ec0c81175519d13e107f9979bb930",
+    "url": "/static/media/project1.4d1ec0c8.png"
+  },
+  {
+    "revision": "5f926e326eac18aa58d2d8cdeabe28bd",
+    "url": "/static/media/project2.5f926e32.png"
+  },
+  {
+    "revision": "67f91601a31e5e7273e2f6d07af06b4c",
+    "url": "/static/media/project3.67f91601.png"
+  },
+  {
+    "revision": "b215b0ed77f8f65c1d867a432b318a3d",
+    "url": "/static/media/project4.b215b0ed.png"
+  },
+  {
+    "revision": "e0f5b90985dc64fd67671ef8682a8ec2",
+    "url": "/static/media/project21.e0f5b909.png"
+  },
+  {
+    "revision": "6ade8e6232f69f259511cd3943e308ef",
+    "url": "/static/media/project31.6ade8e62.png"
+  },
+  {
+    "revision": "d77a661d955b59683fa4d7929db17e51",
+    "url": "/static/media/project11.d77a661d.png"
+  },
+  {
+    "revision": "fb1cb4ada0a457933b311d23401416a2",
+    "url": "/static/media/progress.fb1cb4ad.png"
+  },
+  {
+    "revision": "8042259b785edc392b0ebe2b30382971",
+    "url": "/static/media/project12.8042259b.png"
+  },
+  {
+    "revision": "5dc4e898ee7f877658b6709a1c0c4139",
+    "url": "/static/media/website.5dc4e898.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "781bd398f6eca4dea48db172249738ee",
+    "url": "/index.html"
+  }
+];
